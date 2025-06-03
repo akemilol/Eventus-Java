7e23516cc676731170faea925189b39bf86c1ecf
akemilol <valeriaconcedsantos@gmail.com>
2025-06-03 18:23:32 -0300

    first commit

---
+++ src/main/java/br/com/eventus/eventus_api/EventusApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.eventus.eventus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eventus/eventus_api/EventusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eventus.eventus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
